847cfe52313825be49abb90d9414dfc1684a83f1
xinyuzhao21 <xinyuzhao@umass.edu>
2020-11-23 20:51:17 -0500

    add LingEval97

--- data/__init__.py
+++ data/__init__.py
@@ -6,10 +6,11 @@ from functools import partial
 from data.annotated import TextAnnotation
 from data.iwslt import IWSLTDataset, IWSLTEnJpDataset
 from data.wmt import WMTEnDeDataset, WMTEnFrDataset, WMTEnFrFullDataset, WMTEnRoDataset
+from data.LingEval97 import LingEval97Dataset
 
 DATASETS = {
     _dataset.name(_swap, _annotation): partial(_dataset, swap=_swap, annotation=_annotation)
-    for _dataset in (WMTEnDeDataset, WMTEnFrDataset, WMTEnFrFullDataset, IWSLTDataset, WMTEnRoDataset, IWSLTEnJpDataset)
+    for _dataset in (WMTEnDeDataset, WMTEnFrDataset, WMTEnFrFullDataset, IWSLTDataset, WMTEnRoDataset, IWSLTEnJpDataset,LingEval97Dataset)
     for _annotation in TextAnnotation
     for _swap in [False, True]
 }
